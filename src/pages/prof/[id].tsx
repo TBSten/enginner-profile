7a3b540bce9d26a0dbf9ec628609e7c47e92413b
TBSten <81161390+TBSten@users.noreply.github.com>
2023-02-17 02:58:20 +0900

    prof関連のページを追加

---
+++ src/pages/prof/[id].tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+interface Props {
+}
+const ProfDetailPage: NextPage<Props> = ({ }) => {
+    return (
+        <></>
+    );
+}
+export default ProfDetailPage;
---
+++ src/pages/prof/new.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+interface Props {
+}
+const NewProfPage: NextPage<Props> = ({ }) => {
+    return (
+        <></>
+    );
+}
+export default NewProfPage;

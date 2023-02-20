2a576dc334214f5f16dc5c8d131ec2cf66f36456
TBSten <81161390+TBSten@users.noreply.github.com>
2023-02-21 05:43:06 +0900

    netlifyデプロイに最適化

--- src/lib/server/gcp.ts
+++ src/lib/server/gcp.ts
@@ -4,3 +4,5 @@ export const credentials = {
     client_email: process.env.GCP_SA_CLIENT_EMAIL,
     private_key: process.env.GCP_SA_PRIVATE_KEY?.replaceAll("\\n", "\n"),
 } as const
+
+console.log("init gcp", gcpProjectId, credentials)

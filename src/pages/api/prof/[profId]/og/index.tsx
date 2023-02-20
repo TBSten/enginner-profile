1d9565171c3f5daec64b0829ca2480c2df846934
TBSten <81161390+TBSten@users.noreply.github.com>
2023-02-21 02:36:03 +0900

    OGP画像の修正

--- src/pages/api/prof/[profId]/og/index.tsx
+++ src/pages/api/prof/[profId]/og/index.tsx
@@ -1,12 +1,10 @@
-import { theme as muiTheme } from "@/styles/theme";
-import { ProfSchema } from "@/types";
-import { alpha } from "@mui/material";
+import { ProfItem, ProfSchema } from "@/types";
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 import { CSSProperties } from "react";
 
 export const config = {
-    runtime: 'experimental-edge',
+    runtime: 'edge',
 };
 
 const width = 1200
@@ -17,8 +15,8 @@ const fontSize = {
     lg: Math.min(width / 10, height / 2),
 }
 const maxLength = {
-    name: 10,
-    freeSpace: 15 * 2 * 2,
+    name: 9,
+    skillAndProfItem: 17 * 6,
 }
 
 export default async function handler(req: NextRequest) {
@@ -27,63 +25,78 @@ export default async function handler(req: NextRequest) {
     if (!profId) throw new Error("profId is null")
 
     const prof = await fetchProf(origin, profId)
-    const name = prof.name.length >= maxLength.name
-        ? prof.name.slice(0, maxLength.name - 1) + "..."
-        : prof.name
-    const freeSpace = prof.freeSpace.length >= maxLength.freeSpace
-        ? prof.freeSpace.slice(0, maxLength.freeSpace - 1) + "..."
-        : prof.freeSpace
+    const name = slice(prof.name, maxLength.name)
+    const mainColor = prof.theme.color
+    const skills = prof.skills
+        .filter(sk => sk.appeal)
+        .slice(0, 5)
+        .map(sk => sk.name)
+    const profItems = prof.profItems
+        .filter((pItem): pItem is ProfItem & { value: { type: "text" } } => pItem.appeal && pItem.value.type === "text")
+        .slice(0, 5)
+        .map(pItem => pItem.value.text)
 
     return new ImageResponse(
         (
             <div style={{
                 ...center,
+                ...fill,
                 fontSize: fontSize.md,
                 lineHeight: 0.8,
-                position: "relative",
-                background: muiTheme.palette.getContrastText(prof.theme.color),
+                background: `linear-gradient(45deg, ${mainColor} 30%, #ececec 100%)`,
+                padding: fontSize.sm,
             }}>
-                {/* background */}
-                <img
-                    src={prof.icon}
-                    style={{
-                        position: "absolute",
-                        left: 0,
-                        top: 0,
-                        ...full,
-                        objectFit: "cover",
-                    }}
-                />
-                {/* content */}
                 <div style={{
-                    ...center,
-                    ...full,
-                    color: muiTheme.palette.getContrastText(prof.theme.color),
-                    padding: fontSize.sm,
+                    ...fill,
+                    ...flex("column"),
+                    alignItems: "center",
+                    background: "rgba(255, 255, 255, 0.85)",
+                    padding: fontSize.sm * 0.5,
+                    borderRadius: fontSize.sm,
                 }}>
                     <div style={{
                         ...center,
-                        ...full,
-                        justifyContent: "space-around",
-                        background: alpha(prof.theme.color, 0.95),
-                        color: muiTheme.palette.getContrastText(prof.theme.color),
-                        borderRadius: fontSize.sm,
-                        padding: fontSize.sm,
+                        width: "100%",
+                        fontWeight: "bold",
+                        fontSize: fontSize.lg,
+                        height: fontSize.lg,
+                        flexShrink: 0,
+                    }}>
+                        {name}
+                    </div>
+                    <div style={{
+                        ...flex("row"),
+                        width: "100%",
+                        flexGrow: 1,
                     }}>
                         <div style={{
                             ...center,
-                            width: "100%",
-                            fontSize: fontSize.lg,
-                            fontWeight: "bold",
+                            height: "100%",
+                            padding: fontSize.sm,
                         }}>
-                            {name}
+                            <img
+                                src={prof.icon}
+                                style={{
+                                    width: Math.min(width * 0.25, height * 0.6),
+                                    height: Math.min(width * 0.25, height * 0.6),
+                                    objectFit: "cover",
+                                    borderRadius: fontSize.sm,
+                                }}
+                            />
                         </div>
                         <div style={{
-                            ...center,
+                            ...flex("row", { flexWrap: "wrap" }),
+                            textAlign: "left",
+                            flexGrow: 1,
+                            flexShrink: 1,
                             width: "100%",
                             fontSize: fontSize.sm,
+                            wordBreak: "break-all",
+                            paddingTop: fontSize.md * 0.2,
+                            lineHeight: 1.2,
                         }}>
-                            {freeSpace}
+                            {/* スキル一覧 */}
+                            {slice([...skills, ...profItems].join(" "), maxLength.skillAndProfItem)}
                         </div>
                     </div>
                 </div>
@@ -92,7 +105,7 @@ export default async function handler(req: NextRequest) {
         {
             width,
             height,
-
+            // debug: true,
         }
     )
 }
@@ -105,6 +118,14 @@ const fetchProf = async (origin: string, profId: string) => {
     return prof
 }
 
+const slice = (text: string, maxLen: number, ellipse = "...") => {
+    return (
+        text.length >= maxLen
+            ? text.slice(0, maxLen - 1) + ellipse
+            : text
+    )
+}
+
 // utility properties
 const center: CSSProperties = {
     display: "flex",
@@ -113,10 +134,21 @@ const center: CSSProperties = {
     alignItems: "center",
     textAlign: "center",
 }
-const full: CSSProperties = {
+const fill: CSSProperties = {
     width: "100%",
     height: "100%",
     flexGrow: 1,
 }
-
+const flex = (
+    flexDirection: CSSProperties["flexDirection"] = "column",
+    {
+        flexWrap = "nowrap",
+    }: {
+        flexWrap?: CSSProperties["flexWrap"]
+    } = {}
+): CSSProperties => ({
+    display: "flex",
+    flexDirection,
+    flexWrap,
+})
 
